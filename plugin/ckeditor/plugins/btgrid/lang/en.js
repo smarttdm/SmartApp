6c7769211327080209e31e1835b327ab6be14903
Smart TDM <yong@newtera.com>
2017-09-05 21:04:48 -0700

    Add files via upload

---
+++ plugin/ckeditor/plugins/btgrid/lang/en.js
@@ -0,0 +1,12 @@
+"use strict"
+
+CKEDITOR.plugins.setLang( 'btgrid', 'en', {
+	selNumCols: 'Select number of columns',
+  genNrRows: 'Add number of rows to generate',
+	infoTab: 'Info',
+	createBtGrid: 'Create a Bootstrap grid',
+    insertProperty: "Insert property",
+	editBtGrid: 'Edit Bootstrap grid',
+	numColsError:  'Please select number of columns.',
+	numRowsError: 'Please insert numeric value for Number of rows.',
+} );
---
+++ plugin/ckeditor/plugins/btgrid/lang/zh-cn.js
@@ -0,0 +1,12 @@
+﻿"use strict"
+
+CKEDITOR.plugins.setLang( 'btgrid', 'zh-cn', {
+	selNumCols: '设置栅格列的数量',
+  genNrRows: '设置栅格行的数量',
+	infoTab: '信息',
+	createBtGrid: '创建表单栅格',
+    insertProperty: "添加字段",
+	editBtGrid: '编辑表单栅格',
+	numColsError:  '请选择栅格的列数量',
+	numRowsError: '请输入栅格的行数量',
+} );
