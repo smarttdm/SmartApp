5283e29258eb545d402de503e0840c09e5e2cdb3
Smart TDM <yong@newtera.com>
2018-01-17 20:59:57 -0800

    Add files via upload

---
+++ app/dataviewer/directives/rowSelect.js
@@ -0,0 +1,39 @@
+﻿function rowSelect() {
+    return {
+        require: '^stTable',
+        template: '<input type="checkbox">',
+        scope: {
+            row: '=rowSelect'
+        },
+        link: function (scope, element, attr, ctrl) {
+
+            element.bind('click', function (evt) {
+
+                scope.$apply(function () {
+
+                    ctrl.select(scope.row, 'multiple');
+
+                });
+
+            });
+
+            scope.$watch('row.isSelected', function (newValue) {
+                if (newValue === true) {
+
+                    element.parent().addClass('st-selected');
+                    element.find('input').attr('checked', true);
+
+                } else {
+
+                    element.parent().removeClass('st-selected');
+                    element.find('input').attr('checked', false);
+
+                }
+            });
+        }
+    };
+}
+
+angular
+  .module('app.dataviewer')
+  .directive('rowSelect', rowSelect)
